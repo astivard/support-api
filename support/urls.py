7ba3b5f0277eafc0d54507a18a20b05df86caeec
astivard <mdravitsa@mail.ru>
2022-05-04 11:29:32 +0300

    A little fix

--- support/urls.py
+++ support/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path('api/v1/registration/', UserRegistrationViewSet.as_view({'post': 'create'})),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
